8f0aed87030ab03e9ae66395a3403ec942a4cf1c
Esteban Barrero <ejbh24@stanford.edu>
2026-02-16 00:08:41 -0800

    Adjusted txt for phone

--- components/Header.tsx
+++ components/Header.tsx
@@ -35,7 +35,7 @@ export function Header({
       {/* Optional title overlay */}
       {title && (
         <div className="pointer-events-none absolute inset-0 flex items-center px-6">
-          <h1 className="pl-41 pt-7 md:pt-0 sm:pl-160 text-3xl sm:text-8xl font-medium leading-tight text-black">
+          <h1 className="pl-41 pt-5 md:pt-0 sm:pl-160 text-3xl sm:text-8xl font-medium leading-tight text-black">
             {title}
           </h1>
         </div>
