6b179254498a4dc9b61170e69a02b5ba95a3f6e0
Esteban Barrero <ejbh24@stanford.edu>
2026-02-16 01:31:46 -0800

    Adjusted text locations

--- components/Header.tsx
+++ components/Header.tsx
@@ -26,12 +26,12 @@ export function Header({
       />
 
       {/* Nav overlay */}
-      <div className="absolute top-0 right-0 z-10 pl-6 pr-2 md:pr-6 pt-1 md:pt-6">
+      <div className="absolute top-0 right-0 z-10 pl-[4vw] pr-[2vw] pt-[2vw] md:pr-6 md:pt-6">
         <nav
-          className={`flex items-center gap-3 md:gap-7 text-sm md:text-lg ${
-            navClassName ?? "text-black"
+          className={`flex items-center gap-[4vw] md:gap-7 text-[3.5vw] md:text-lg ${
+             navClassName ?? "text-black"
           }`}
-        >
+        >   
           <Link href="/">Home</Link>
           <Link href="/about">About</Link>
           <Link href="/#studio">From the Studio</Link>
@@ -41,7 +41,7 @@ export function Header({
       {/* Optional title overlay */}
       {title && (
         <div className="pointer-events-none absolute inset-0 flex items-center px-6">
-          <h1 className="pl-[33vw] pt-[1vw] md:pt-0 sm:pl-160 text-[7vw] sm:text-8xl font-medium leading-tight text-black">
+          <h1 className="pl-[33vw] pt-[1vw] md:pt-0 sm:pl-[38vw] text-[7vw] sm:text-[7vw] font-medium leading-tight text-black">
             {title}
           </h1>
         </div>
